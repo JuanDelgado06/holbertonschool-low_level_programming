a47341fd7aa31689bc44db6182cf17cca4eaeea5
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-06-11 16:33:59 +0000

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print out a message
+ * Use the function puts
+ * You are not allowed to use printf
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+	return 0;
+}
