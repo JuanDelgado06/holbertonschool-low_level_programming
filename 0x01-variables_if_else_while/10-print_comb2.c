cd40e40b7ff035f31f0d79797c0fc59dcd0560c6
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-06-12 04:30:54 +0000

    fix problem file 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,29 +1,25 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the numbers from 00 to 99
+ *
+ * Return: 0
+ */
 int main(void)
 {
-int one, two;
+	int one, two;
 
-for (one = '0'; one <= '9'; one++)
-{
-for (two = '0'; two <= '9'; two++)
-{
-putchar(one);
-putchar(two);
-if (two != '9' && one != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-
-putchar('\n');
-return (0);
+	for (one = '0'; one <= '9'; one++)
+		for (two = '0'; two <= '9'; two++)
+		{
+			putchar(one);
+			putchar(two);
+			if (one != '9' || two != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
 }
