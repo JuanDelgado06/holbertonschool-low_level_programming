f7d93b3918b86f12807e4c8d8f4482e54703b167
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-06-12 03:21:57 +0000

    add file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - count number
+ *
+ * return 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
