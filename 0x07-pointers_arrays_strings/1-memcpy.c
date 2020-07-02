998c9f79ae8efe327c41e2fb24adac5fcd710134
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-07-02 03:27:02 +0000

    add files default

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,15 +11,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char **p;
+	char *aux = dest;
 
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	p = &dest;
-
-	return (*p);
+	for (i = 0; i < n; i++)
+		*dest++ = *src++;
+	return (aux);
 }
