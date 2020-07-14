fb61be352716475ce2fa4da7e38377ad5a673775
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-07-14 04:35:57 +0000

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct containing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+#endif
