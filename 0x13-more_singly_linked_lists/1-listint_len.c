4724cd1195021e734106168898093608f8e25be5
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-07-27 23:05:10 +0000

    add task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * listint_len - Return the number of elements in a listint_t list
+ * @h: The list
+ *
+ * Return: The number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
