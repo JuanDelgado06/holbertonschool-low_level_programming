8b4997b4461232f16e8f2f89042e7559acf2aceb
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-07-31 19:46:29 +0000

    add task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Convert a binary number to an unsigned int
+ * @b: A string of 0 and 1 chars
+ *
+ * Return: The converted number if successful, 0 otherwise
+ */
